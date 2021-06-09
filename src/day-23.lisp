b45912bea55a1ca9373750d579128c4b1d7001d8
Maris Orbidans <maris.orbidans@onthemarket.com>
2021-06-09 21:57:03 +0100

    refactor

--- src/day-23.lisp
+++ src/day-23.lisp
@@ -7,8 +7,6 @@
 
 (defparameter *nat* nil)
 (defparameter *prev-nat-y* nil)
-(defparameter *computers* nil)
-(defparameter *queues* nil)
 
 (define-constant +number-of-computers+ 50)
 (define-constant +nat-address+ 255)
@@ -20,66 +18,66 @@
   (let ((code (read-input))
 	(*nat* nil)
 	(*prev-nat-y* nil)
-	(*computers* (make-array +number-of-computers+))
-	(*queues* (make-array +number-of-computers+)))
+	(computers (make-array +number-of-computers+))
+	(queues (make-array +number-of-computers+)))
     (iter
       (for i :below +number-of-computers+)
-      (setf (svref *computers* i) (copy-code code))
+      (setf (svref computers i) (copy-code code))
       (let ((io-queue (make-queue :simple-queue)))
-	(setf (svref *queues* i) io-queue)
+	(setf (svref queues i) io-queue)
 	(qpush io-queue i)))
-    (funcall run-fn)))
+    (funcall run-fn computers queues)))
 
 (defun solution-1 ()
   (cdr (solution #'run-computers-1)))
 
-(defun run-computer (idx &optional is-input? outputs)
-  (let* ((program (svref *computers* idx))
-	 (queue (svref *queues* idx))
+(defun run-computer (computers queues idx &optional is-input? outputs)
+  (let* ((program (svref computers idx))
+	 (queue (svref queues idx))
 	 (input (when is-input?
 		  (qpop queue)))
 	 (inputs (when is-input?
 		   (list (or input -1))))
 	 (result (run-program program :input inputs :max-outputs 1)))
     (ecase (program-status program)
-      (:running (run-computer idx nil (cons result outputs)))
+      (:running (run-computer computers queues idx nil (cons result outputs)))
       (:input (if (and is-input? (null input))
 		  (nreverse outputs)
-		  (run-computer idx t outputs))))))
+		  (run-computer computers queues idx t outputs))))))
 
-(defun run-computers-1 ()
+(defun run-computers-1 (computers queues)
   (iter
     (for i :below +number-of-computers+)
-    (-> (run-computer i)
-      (dispatch-packets)))
-  (or *nat* (run-computers-1)))
+    (->> (run-computer computers queues i)
+      (dispatch-packets queues)))
+  (or *nat* (run-computers-1 computers queues)))
 
-(defun send-nat (packets-sent)
+(defun send-nat (queues packets-sent)
   (when (and (zerop packets-sent) *nat*)
     (destructuring-bind (x . y) *nat*
-      (qpush (svref *queues* 0) x)
-      (qpush (svref *queues* 0) y)
+      (qpush (svref queues 0) x)
+      (qpush (svref queues 0) y)
       (prog1 (when (eql *prev-nat-y* y) y)
 	(setq *prev-nat-y* y)))))
 
-(defun dispatch-packets (packets)
+(defun dispatch-packets (queues packets)
   (when packets
     (destructuring-bind (address x y &rest rest) packets
       (if (eql address +nat-address+)
 	  (setq *nat* (cons x y))
 	  (progn
-	    (qpush (svref *queues* address) x)
-	    (qpush (svref *queues* address) y)))
-      (dispatch-packets rest))))
+	    (qpush (svref queues address) x)
+	    (qpush (svref queues address) y)))
+      (dispatch-packets queues rest))))
 
-(defun run-computers-2 ()
+(defun run-computers-2 (computers queues)
   (let ((packets-sent 0))
     (iter
       (for i :below +number-of-computers+)
-      (let ((packets (run-computer i)))
-	(dispatch-packets packets)
+      (let ((packets (run-computer computers queues i)))
+	(dispatch-packets queues packets)
 	(incf packets-sent (length packets))))
-    (or (send-nat packets-sent) (run-computers-2))))
+    (or (send-nat queues packets-sent) (run-computers-2 computers queues))))
 
 (defun solution-2 ()
   (solution #'run-computers-2))
